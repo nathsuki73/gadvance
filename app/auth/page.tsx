b7b5b69ab9c8b6f6ac912cb7db24bc56bc30c35e
Tan <nathsegovia73@gmail.com>
2026-02-24 23:05:04 +0800

    Updated the login design

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -19,7 +19,7 @@ const Authentication = () => {
             </div>
 
             {/* Header */}
-            <h1 className="text-4xl font-extrabold text-gray-900 mb-2">
+            <h1 className="text-4xl font-bold text-gray-900 mb-2">
               Hello, <br /> Welcome Back
             </h1>
             <p className="text-gray-400 mb-10 text-sm">
