b947f7dffd481bbb99cc08527e656c6f17092564
Tan <nathsegovia73@gmail.com>
2026-03-08 22:16:17 +0800

    Add AuthLayout component with SessionProvider for authentication context

---
+++ app/auth/layout.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SessionProvider>
+      <div className="">{children}</div>
+    </SessionProvider>
+  );
+}
