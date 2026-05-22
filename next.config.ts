624165756d98fbd5c03228b96ff683c851011185
Tan <nathsegovia73@gmail.com>
2026-05-22 21:56:11 +0800

    feat(next.config): add local development image loading configuration

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/**",
+      },
     ],
   },
   experimental: {
