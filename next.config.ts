c2fc82ed24d4307f23bb7802c08e34ddd9b7feda
Tan <nathsegovia73@gmail.com>
2026-07-23 23:29:35 +0800

    feat: add output configuration to next.js and update deploy script in package.json

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 const nextConfig: NextConfig = {
   // reactStrictMode: false,
+  output: "export",
   images: {
     dangerouslyAllowLocalIP: true,
     remotePatterns: [
